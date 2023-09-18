50105d73eb0221d4486a64e394b33a1e077998ed
Yonhatan Ramirez <yramirezc@intergrupo.com>
2023-09-18 11:57:58 -0500

    Se agregan los metodos para consumir procedimientos almacenados con dapper

--- PersistenciaSQL_Dapper/Program.cs
+++ PersistenciaSQL_Dapper/Program.cs
@@ -4,14 +4,63 @@
 using Dapper;
 using Microsoft.Data.SqlClient;
 using PersistenciaSQL_Dapper;
+using System.Data;
 
-using (SqlConnection sql = new SqlConnection("Data Source=;Initial Catalog=WebApiNet6;Integrated Security=true;Connect Timeout=30;Encrypt=False"))
+using (SqlConnection sql = new SqlConnection("Data Source=SWO5CD208FXVW\\SQLEXPRESS;Initial Catalog=WebApiNet6;Integrated Security=true;Connect Timeout=30;Encrypt=False"))
 {
-    var consulta = "SELECT * FROM PRUEBA";
-    var lista = sql.Query<Prueba>(consulta);
+    //PROCEDIMIENTO ALMACENADO INSERTAR
+    //Prueba prueba = new Prueba();
+    //prueba.Nombre = "XXXXXX";
+    //var param = new DynamicParameters();
+    //param.Add("@Nombre", prueba.Nombre);
+    //sql.ExecuteScalar("EjemploInsert", param, commandType: CommandType.StoredProcedure);
 
-    foreach (var item in lista)
+    //PRODEDIMIENTO ALMACENADO SIN PARAMETROS
+    var r = sql.ExecuteReader("Ejemplo", commandType: CommandType.StoredProcedure);
+    while (r.Read())
     {
-        Console.WriteLine(item.Id + "  " + item.Nombre);
+        Console.WriteLine(r["Nombre"]);
     }
+
+    r.Close();
+
+    Console.WriteLine("------------------");
+
+    //PRODEDIMIENTO ALMACENADO CON PARAMETROS
+    var parametro = new DynamicParameters();
+    parametro.Add("@id", 1);
+    r = sql.ExecuteReader("Ejemplo", parametro, commandType: CommandType.StoredProcedure);
+
+    while (r.Read())
+    {
+        Console.WriteLine(r["Nombre"]);
+    }
+    r.Close();
+
+    ////INSERTAR DATOS
+    //Prueba prueba = new Prueba();
+    //prueba.Nombre = "Antonio";
+    //string insertar = "INSERT INTO dbo.Prueba(Nombre) values (@Nombre)";
+    //var res = sql.Execute(insertar, prueba);
+
+    ////ACTUALIZAR
+    //prueba.Nombre = "Antonio3";
+    //string actualizar = "UPDATE dbo.Prueba SET Nombre=@Nombre where Nombre = 'Antonio'";
+    //res = sql.Execute(actualizar, prueba);
+
+    ////BORRAR
+    //prueba.Nombre = "Antonio3";
+    //string borrar = "DELETE FROM dbo.Prueba where Nombre = @Nombre";
+    //res = sql.Execute(borrar, prueba);
+
+    ////LISTAR DATOS
+    //var consulta = "SELECT * FROM PRUEBA";
+    //var lista = sql.Query<Prueba>(consulta);
+
+    //foreach (var item in lista)
+    //{
+    //    Console.WriteLine(item.Id + "  " + item.Nombre);
+    //}
+
+
 }
